f662888d22f4b5af0467b26bc72066e83bc72dd7
pancomplex <renaissance21c@gmail.com>
2021-11-02 23:04:14 +0900

    11.02 / Default Setting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import styled from "styled-components";
+import * as color from "./common/color";
+const Logo = styled.h1`
+  color: ${color.primary};
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Logo>Velog</Logo>
     </div>
   );
 }
---
+++ src/common/color.js
@@ -0,0 +1,2 @@
+export const textColor = "#333";
+export const primary = "#B39DDB";
